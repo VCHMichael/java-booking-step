12bf3aec84f90b7c6fadaab5fc6626bbcaea669b
VCyx <69753522+VCyx@users.noreply.github.com>
2021-08-13 15:04:33 +0300

    Feature/structure (#15)
    * initial ReservationDao
    
    * booking logic finished, initial tests
    
    * reservation tests, logger
    
    * merged main
    
    * project structure changed
    
    * file structure
    
    * logger changes
    
    * filepath changes

--- src/main/java/app/logger/Logger.java
+++ src/main/java/app/logger/Logger.java
@@ -1,6 +1,5 @@
 package app.logger;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -8,26 +7,22 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Logger {
-    private static final String LOG_FILE_PATH = "src/main/resources/log.txt";
+    private final String logFilePath = Objects.requireNonNull(this.getClass().getClassLoader().getResource("log.txt")).toString();
     private final Class clazz;
 
     public Logger(Class clazz) {
-        File log = new File(LOG_FILE_PATH);
-        try {
-            log.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         this.clazz = clazz;
     }
 
     public void info(String message) {
         String decoratedMessage = "INFO: " + new Date() + " [" + clazz.getSimpleName() + "] - " + message + "\n";
         try {
-            Files.write(Paths.get(LOG_FILE_PATH), decoratedMessage.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            String path = Objects.requireNonNull(this.getClass().getClassLoader().getResource("log.txt")).toString();
+            Files.write(Paths.get(path), decoratedMessage.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
@@ -39,7 +34,7 @@ public class Logger {
                 .collect(Collectors.joining(" \n"));
         String decoratedMessage = "ERROR: " + new Date() + " [" + clazz.getSimpleName() + "] - " + message + "\n" + stackTrace + "\n";
         try {
-            Files.write(Paths.get(LOG_FILE_PATH), decoratedMessage.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            Files.write(Paths.get(logFilePath), decoratedMessage.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
@@ -48,7 +43,7 @@ public class Logger {
     public void error(String message) {
         String decoratedMessage = "ERROR: " + new Date() + " [" + clazz.getSimpleName() + "] - " + message + "\n";
         try {
-            Files.write(Paths.get(LOG_FILE_PATH), decoratedMessage.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            Files.write(Paths.get(logFilePath), decoratedMessage.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
